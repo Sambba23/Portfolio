6ac4a737206ceccd465bb3ff3081497da591654e
Lenkola Sampo <sampo.lenkola@aalto.fi>
2023-11-13 20:33:47 +0200

    Window scaling working and started with the introtext.js

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,8 +1,10 @@
 import './style.css';
-import { initializeRenderer } from './components/sceneSetup';
+import { camera, initializeRenderer } from './components/sceneSetup';
 import { setupLights } from './components/lighting';
 import { populateStars } from './components/stars';
 import { animate } from './components/animation';
+import { createText } from './components/introText';
+import { scene } from './components/sceneSetup';
 
 // Initialize the renderer and attach it to your #bg canvas
 const renderer = initializeRenderer(document.querySelector('#bg'));
@@ -13,5 +15,14 @@ setupLights();
 // Add stars to the scene
 populateStars();
 
+// Add text for the landing page
+createText(scene)
+
+// If window size is modified, it autoscales
+window.addEventListener('resize', function() {
+  camera.aspect = this.window.innerWidth / this.window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+})
 // Start the animation loop
-animate(renderer);
+animate(renderer)
